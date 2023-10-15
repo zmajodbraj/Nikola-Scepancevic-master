f1e97c4f194d0964a2a409782214ed15459d9781
zmajodbraj <116656116+zmajodbraj@users.noreply.github.com>
2023-10-15 19:03:36 +0200

    Update dublincoretxt.php

--- dublincoretxt.php
+++ dublincoretxt.php
@@ -1,24 +1,27 @@
 <?php
 session_start();
-$sadrzaj="";
-$knjige3=$_SESSION["MyVar"];	
-/*  Drugi prolazak kroz simple xml parser */
-$xml3 = simplexml_load_string($knjige3) or die("Ne mogu da otvorim string knjige3 za parsiranje.");
-$hdr='<?xml version="1.0" encoding="utf-8" ?>\n';
-$sadrzaj.=$hdr; //dodajemo zaglavlje xml fajla
 
-$str="<records>\n";          
-$sadrzaj.=$str; //dodamo otvoreni tag za sve zapise
-       foreach($xml3->children() as $zapisi){
-	  /*izdvajaju se podaci o naslovu, izdanju, izdavanju, autorima, i materijalnom opisu. */ 
-    $naslov="";$izd="";$izdsed="";$izdnaziv=""; $izdgod="";$a1="";$a2="";$matopis="";$strcont1="";$strcont2="";
-    $str="  <record>\n";
-    $sadrzaj.=$str; //dodamo otvoreni tag za svaki pojedinacni zapis
+$knjige3=$_SESSION["MyVar"];
+
+$xml3 = simplexml_load_string($knjige3) or die("Ne mogu da otvorim string knjige3 za parsiranje.");
+$str="";
+//zapocinjemo xml fajl sa zaglavljem
+$hdr="<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n";
+$str.=$hdr;
+//otvaramo tag za skup zapisa i povezujemo se sa definicijom elemenata Dublin core
+$str.="<records xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n";          
+  foreach($xml3->children() as $zapisi){
+    $naslov="";$izd="";$izdsed="";$izdnaziv=""; $izdgod="";$a1="";$a2="";$odrednica="";$mater="";$izdcel="";$sifzapisa="";$napomene="";$isbn=""; $inv="";
+    $str.="  <record>\n";//dodajemo otvoreni tag za pojedinacni zapis
+    $strcont="";
     foreach($zapisi->children() as $polja){
+     
        $ozn_polje=$polja->getName();
        $polje=$polja;
+//       echo $ozn_polje . " " . $polje . "<br>";
        switch ($ozn_polje){
          case "p200a" :
+           $odrednica=$polje;
            $naslov=$polje;
            break;
          case "p200d" :
@@ -40,13 +43,13 @@ $sadrzaj.=$str; //dodamo otvoreni tag za sve zapise
             $izdgod=$polje;
             break;
          case "p215a" :
-            $matopis=$polje;
+            $mater=$polje;
             break;
          case "p215c" :
-            $matopis .= " : " . $polje;
+            $mater .= " : " . $polje;
             break;
          case "p215d" :
-            $matopis .= " ; " . $polje;
+            $mater .= " ; " . $polje;
             break;
          case "p215e" :
             $mater .= " + " . $polje;
@@ -58,63 +61,74 @@ $sadrzaj.=$str; //dodamo otvoreni tag za sve zapise
             $auti=$polje;
             $autp=$a1;
             $a1=$auti . " " . $autp;
-            $strcont1="    <dc_author>" . $a1 . "</dc_author>\n";
+            $strauth="    <dc:contributor.author>" . $a1 . "</dc:contributor.author>\n";
             break;
          case "p701a" :
-            $prezime=$polje;
+           $prezime=$polje;
            break;
          case "p701b" :
             $auti=$polje;
             $autp=$prezime;
-            $strcont2 .= "    <dc_author>" . $auti . " " . $autp . "</dc_author>\n";
+            $strauth .= "    <dc:contributor.author>" . $auti . " " . $autp . "</dc:contributor.author>\n";
             break;
          case "p710a" :
             $a1=$polje;
             break;
          case "p710d" :
-            $a1 .= " (" . $polje;
+            $a1.= " (" . $polje;
             break;
          case "p710f" :
-            $a1 .= " ; " . $polje;
+            $a1.= " ; " . $polje;
             break;
          case "p710e" :
             $a1 .= " ; " . $polje . ")";
-            $strcont1 .= "    <dc_author>" . $a1 . "</dc_author>\n" ;
+            $strauth .= "    <dc:contributor.author>" . $a1 . "</dc:contributor.author>\n";
+            break;
+         case "p225a" :
+            $izdcel= $polje;
+            break;
+	 case "p225v" : 
+            $izdcel .= " ; " . $polje;
+	    break;
+         case "p327a" :
+            $napomene .= $polje;
+            $napomene .= ". ";
+            break;
+         case "p010a" :
+            $isbn= $polje;
+            break;
+         case "p995f" :
+            $inv .=$polje;
+            break;
+         case "p995k" :
+            $inv .= "=" . $polje . "  ";
             break;
          default :
         
         }
        }
-	  /* formira se Dublin core format  */ 
-       $str="    <dc_creator>Bibloteka Matematickog fakulteta</dc_creator>\n";
-       $sadrzaj.=$str;
-       $str1=date("d.m.Y");		
-       $str="    <dc_date>" . $str1 . ". </dc_date>\n";
-       $sadrzaj.=$str;
-       $str="    <dc_publisher>" . $izdsed . " : " . $izdnaziv . ", " . $izdgod . "</dc_publisher>\n";
-       $sadrzaj.=$str;
-       $sadrzaj.=$strcont1;
-       $sadrzaj.=$strcont2;
-       $str="    <dc_format>" . $matopis . "</dc_format>\n";
-       $sadrzaj.=$str;
-       $str="    <dc_type>Text </dc_type>\n";
-       $sadrzaj.=$str);
-       $str="    <dc_title>" . $naslov . "</dc_title>\n";
-       $sadrzaj.=$str;
-       $str="    <dc_subject></dc_subject>\n";
-       $sadrzaj.=$str; 
-       $str="    <dc_description></dc_description>\n";
-       $sadrzaj.=$str;
-       $str="  </record>\n";
-       $sadrzaj.=$str; // dodamo zatvoreni tag za svaki pojedinacni zapis,
+       $str.="    <dc:creator>Biblioteka Matemati&#269;kog fakulteta</dc:creator>\n";
+       $str1=$izdgod;		
+       $str.="    <dc:date>" . $str1 . ". </dc:date>\n";
+       
+       $str.="    <dc:publisher>" . $izdsed . " : " . $izdnaziv . "</dc:publisher>\n";
+       
+       $str.=$strauth; 
+       $str.="    <dc:format>" . $mater . "</dc:format>\n";
+       $str.="    <dc:type>Text </dc:type>\n";
+       $str.="    <dc:title>" . $naslov . "</dc:title>\n";
+       $str.="    <dc:subject></dc:subject>\n";
+       $str.="    <dc:description></dc:description>\n";
+       $str.="  </record>\n";//zatvaramo tag za pojedinacni zapis
+       
 
      }
-     $str="</records>\n"; //dodamo zatvoreni tag za sve zapise
-     $sadrzaj.=$str;
-     header('Content-disposition: attachment; filename=dublincore.xml');
-     header ("Content-Type:  text/xml");
-     echo $sadrzaj;
+     $str.="</records>\n"; //zatvaramo tag za skup zapisa
+   
+     header('Content-disposition: attachment; filename=dublincore.xml');//kreiramo izlazni fajl 
+     header('Content-Type:  text/xml');
+     echo $str;//ispisujemo sadrzaj u fajl
 
-   session_unset($_SESSION["MyVal"]);//oslobadjamo $_SESSION promenljivu,
-   session_destroy(); //uklanjamo sesiju
+     session_unset($_SESSION["MyVar"]);
+     session_destroy();
 ?>
